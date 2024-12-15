f0f7a1a7cd1de94cc446a91ba88aa5a65b6db22d
ThiagoDelgado <thiagodelgado.dev@gmail.com>
2024-12-15 11:54:19 -0300

    Jest init config & tests user service and controller

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,20 +1,58 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
-describe('UserController', () => {
-  let controller: UserController;
+describe('UserController (e2e)', () => {
+  let userController: UserController;
+  let userService: UserService;
+
+  const mockUserRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    save: jest.fn(),
+    delete: jest.fn(),
+  };
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    userService = moduleRef.get<UserService>(UserService);
+    userController = new UserController(userService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(userController).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const mockUsers: User[] = [
+        {
+          id: '1',
+          email: 'john@example.com',
+          password: 'hashedpassword',
+          workspaces: [],
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          fullName: 'John Doe',
+        },
+      ];
+      jest.spyOn(userService, 'findAll').mockResolvedValue(mockUsers);
+
+      const result = await userController.findAll();
+      expect(result).toEqual(mockUsers);
+      expect(userService.findAll).toHaveBeenCalled();
+    });
   });
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UserService', () => {
   let service: UserService;
+  let userRepository: Repository<User>;
+
+  const mockUserRepository = {
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('Should return a user by ID', async () => {
+      const mockUser = {
+        id: '123',
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        workspaces: [],
+      };
+
+      mockUserRepository.findOne.mockRejectedValue(mockUser);
+
+      const result = await service.findOne('123');
+      expect(result).toEqual(mockUser);
+      expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+        where: { id: '123' },
+        relations: ['workspaces'],
+      });
+    });
   });
 });
