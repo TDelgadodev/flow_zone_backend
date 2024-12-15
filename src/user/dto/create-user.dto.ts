d44b8609c067081d16149d727bb8f1c42de5f46a
ThiagoDelgado <thiagodelgado.dev@gmail.com>
2024-12-15 11:54:00 -0300

    init service & controller to User Module

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(6) 
+  password: string;
+
+  fullName?: string;
+}
